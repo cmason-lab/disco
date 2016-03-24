a2c0d4a0695296e3519e97866fb7993f6695c7d8
priya-vijay <priyaaa.v@gmail.com>
2016-03-23 22:04:28 -0400

    more restructuring mods

--- lib/disco/group.py
+++ lib/disco/group.py
@@ -3,6 +3,7 @@ import pandas as pd
 import glob
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 # import multiprocessing as mp
 
--- setup.py
+++ setup.py
@@ -2,20 +2,15 @@ from distutils.core import setup
 
 setup(
       name='disco',
-      version='0.0.1',
+      version='0.1.0',
       description='Python tools for analyzing single cell alternative splicing',
       author='Priyanka Vijay',
       author_email='prv2004',
       url='',
-      package_dir={'': 'lib'},
-      entry_points={
-                    'console_scripts': [
-                                        'disco = disco.disco:main',
-                                        'Disco = disco.disco:main',
-                                        ]
-                   },
       packages=[
                 'disco',
                 ],
+      package_dir={'disco': 'lib/disco'},
+      scripts=['scripts/disco'],
       requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'seaborn']
      )
