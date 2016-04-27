7bc7ee5a032a2d46916b281ee1924104bdab3bd2
Priyanka Vijay <pv225@cornell.edu>
2016-04-26 21:04:44 -0400

    new plotting functions for bulk and minor updates

--- disco/__init__.py
+++ disco/__init__.py
@@ -1,3 +1,4 @@
 from .group import Disco
 from .compare import stat_test, getsig
-from .plot import plotsig_violin, plotviolin_1samp, plotbulk
+# from .plot import plotsig_violin, plotviolin_1samp, plotbulk, plotsinglebulk
+from .plot import *
--- disco/plot.py
+++ disco/plot.py
@@ -15,8 +15,11 @@ def plotsig_violin(sigks, statsres, sciso1, sciso2, outfile, sampname1, sampname
     alldatadf2 = sciso2.alldatadf
     alldatadf1["group"] = pd.Series(np.repeat(sampname1, alldatadf1.shape[0]), index=alldatadf1.index)
     alldatadf2["group"] = pd.Series(np.repeat(sampname2, alldatadf2.shape[0]), index=alldatadf2.index)
-    genestoplot = sigks["Ensemble_ID"].unique()
+    genestoplot = sigks.sort('median_shift')["Ensemble_ID"].unique()
     print "Plotting ", len(genestoplot), " genes"
+    if len(genestoplot) > 100:
+        genestoplot = genestoplot[0:100]
+        print "plotting top 100 genes (by median shift) of ", len(genestoplot), "total significant genes"
     for gene in genestoplot:
         # print gene
         genedf1 = alldatadf1[alldatadf1["event_name"] == gene]
@@ -134,6 +137,91 @@ def plotbulk(sigks, statsres, disco1, disco2, sampname1, sampname2, color1, colo
     print "Saved plots to", outfile
     sns.set()
     return
+#
+#
+#
+# #incomplete:
+
+
+def plotsinglebulk(sigks, statsres, disco1, sampname1, color1, outfile):
+    sns.set(style="dark", palette="muted", color_codes=True, font_scale=1.5)
+    plotspdf = pdf.PdfPages(outfile)
+    alldatadf1 = disco1.alldatadf
+    #alldatadf2 = disco2.alldatadf
+    alldatadf1["group"] = pd.Series(np.repeat(sampname1, alldatadf1.shape[0]), index=alldatadf1.index)
+    #alldatadf2["group"] = pd.Series(np.repeat(sampname2, alldatadf2.shape[0]), index=alldatadf2.index)
+    genestoplot = sigks["Ensemble_ID"].unique()
+    for gene in genestoplot:
+        genedf1 = alldatadf1[alldatadf1["event_name"] == gene]
+        genedf1.head()
+        #genedf2 = alldatadf2[alldatadf2["event_name"] == gene]
+        #genedf2.head()
+        #merged = pd.concat([genedf1, genedf2])
+        merged = genedf1
+        merged2 = merged.apply(_renamer, axis=1, args=(sigks.index, statsres.index, statsres['Isoform_Function']))
+        sns.barplot(x="newlabel", y="psi_i", hue="group", data=merged2,
+                    palette={sampname1: color1})
+        plt.ylim(0, 1)
+        annrow = sigks[sigks["Ensemble_ID"] == gene].iloc[0]
+        if annrow["Gene_Symbol"] is None:
+            plt.title(gene)
+            print gene
+        else:
+            plt.title(annrow["Gene_Symbol"]+"\n"+annrow["Gene_Name"]+"; "+annrow["Locus"])
+            print annrow["Gene_Symbol"]
+        plt.ylabel("Distribution of Isoform Expression")
+        plt.xlabel("Isoform ( - tested in sc, * significant in sc )")
+        plt.legend(loc='upper left', bbox_to_anchor=(1.0, 0.9))
+        plt.subplots_adjust(top=0.88, right=0.7, bottom=0.2)
+        plotspdf.savefig()
+        plt.close()
+    plotspdf.close()
+    print "Saved plots to", outfile
+    sns.set()
+    return
+
+
+def plot3bulk(sigks, statsres, disco1, disco2, disco3, sampname1, sampname2, sampname3,
+              color1, color2, color3, outfile):
+    sns.set(style="dark", palette="muted", color_codes=True, font_scale=1.5)
+    plotspdf = pdf.PdfPages(outfile)
+    alldatadf1 = disco1.alldatadf
+    alldatadf2 = disco2.alldatadf
+    alldatadf3 = disco3.alldatadf
+    alldatadf1["group"] = pd.Series(np.repeat(sampname1, alldatadf1.shape[0]), index=alldatadf1.index)
+    alldatadf2["group"] = pd.Series(np.repeat(sampname2, alldatadf2.shape[0]), index=alldatadf2.index)
+    alldatadf3["group"] = pd.Series(np.repeat(sampname3, alldatadf3.shape[0]), index=alldatadf3.index)
+    genestoplot = sigks["Ensemble_ID"].unique()
+    for gene in genestoplot:
+        genedf1 = alldatadf1[alldatadf1["event_name"] == gene]
+        genedf1.head()
+        genedf2 = alldatadf2[alldatadf2["event_name"] == gene]
+        genedf2.head()
+        genedf3 = alldatadf3[alldatadf3["event_name"] == gene]
+        genedf3.head()
+        merged = pd.concat([genedf1, genedf2, genedf3])
+        merged2 = merged.apply(_renamer, axis=1, args=(sigks.index, statsres.index, statsres['Isoform_Function']))
+        sns.barplot(x="newlabel", y="psi_i", hue="group", data=merged2,
+                    palette={sampname1: color1, sampname2: color2, sampname3: color3})
+        plt.ylim(0, 1)
+        annrow = sigks[sigks["Ensemble_ID"] == gene].iloc[0]
+        if annrow["Gene_Symbol"] is None:
+            plt.title(gene)
+            print gene
+        else:
+            plt.title(annrow["Gene_Symbol"]+"\n"+annrow["Gene_Name"]+"; "+annrow["Locus"])
+            print annrow["Gene_Symbol"]
+        plt.ylabel("Distribution of Isoform Expression")
+        plt.xlabel("Isoform ( - tested in sc, * significant in sc )")
+        plt.legend(loc='upper left', bbox_to_anchor=(1.0, 0.9))
+        plt.subplots_adjust(top=0.88, right=0.7, bottom=0.2)
+        plotspdf.savefig()
+        plt.close()
+    plotspdf.close()
+    print "Saved plots to", outfile
+    sns.set()
+    return
+
 
 # def plotsig_hist(sigks, statsres, disco1, disco2, outfile, sampname1, sampname2):
 #     pass
