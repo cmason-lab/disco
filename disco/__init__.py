aab4b0abe676579826ee84b28858297136fe20a3
Priyanka Vijay <pv225@cornell.edu>
2017-01-15 08:49:12 -0500

    pvalue

--- disco/__init__.py
+++ disco/__init__.py
@@ -1,4 +1,4 @@
 from .group import Disco
-from .compare import stat_test, getsig
+from .compare import stat_test, getsig, filterdf
 # from .plot import plotsig_violin, plotviolin_1samp, plotbulk, plotsinglebulk
 from .plot import *
